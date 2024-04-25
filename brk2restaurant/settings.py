2a9775ea349851156ea652b73fbb7ad4c4ff7688
Tesfaye Wolde <tesfekadu21@gmail.com>
2024-04-25 09:39:40 +0000

    migrate booking model

--- brk2restaurant/settings.py
+++ brk2restaurant/settings.py
@@ -55,16 +55,6 @@ SITE_ID = 1
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# Details of message alerts
-MESSAGE_TAGS = {
-    messages.DEBUG: 'alert-info',
-    messages.INFO: 'alert-info',
-    messages.SUCCESS: 'alert-success',
-    messages.WARNING: 'alert-warning',
-    messages.ERROR: 'alert-danger',
-}
-
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
