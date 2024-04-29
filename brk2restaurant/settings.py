23c20bafba81eae44ba7cba6ff103ec18fb5ea09
Tesfaye Wolde <tesfekadu21@gmail.com>
2024-04-29 08:18:29 +0000

    fix allauth rendering issue

--- brk2restaurant/settings.py
+++ brk2restaurant/settings.py
@@ -85,7 +85,7 @@ ROOT_URLCONF = 'brk2restaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [TEMPLATES_DIR,  os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
